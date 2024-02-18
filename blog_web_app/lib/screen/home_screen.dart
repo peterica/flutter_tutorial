228170fab6b0a6b0b703da785a3bf340f6d8599e
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-19 08:33:32 +0900

    홈 버튼 구현하기

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  // 컨트롤러 변수
+  WebViewController? controller;
+
   // 생성자
   HomeScreen({Key? key}) : super(key: key);
 
@@ -10,7 +13,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 홈 버튼 구현하기
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('피터의 개발이야기'),
+        centerTitle: true,
+
+        // 홈버튼 액션
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (controller != null) {
+                controller!.loadUrl('https://peterica.tistory.com');
+              }
+            },
+            // 홈버튼 아이콘 생성
+            icon: Icon(Icons.home)),
+        ],
+      ),
       body: WebView(
+        // 위젯에 컨트롤러 생성
+        onWebViewCreated: (WebViewController controller){
+          this.controller = controller;
+        },
         initialUrl: 'https://peterica.tistory.com',
         javascriptMode: JavascriptMode.unrestricted,
       ),
