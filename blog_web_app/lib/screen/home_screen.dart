7518d20e5d329f3942e22c1c966681ed8cd664e7
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-19 08:09:29 +0900

    Screen 폴더 생성 및 안드로이드 http 접근 권한 설정

---
+++ blog_web_app/lib/screen/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  // 생성자
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home Screen'),
+    );
+  }
+}
