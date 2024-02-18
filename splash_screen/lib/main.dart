5b80a6e2a1d9d2be838d8cd88b2be39032df3a2d
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-18 19:17:21 +0900

    Container와 BoxDecoration 사용방법

--- splash_screen/lib/main.dart
+++ splash_screen/lib/main.dart
@@ -4,14 +4,20 @@ void main() {
   runApp(SplashScreen());
 }
 
+// Container와 BoxDecoration 사용방법
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Slash Screen'),
-        ),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+          child: Center(
+            child: Text('Splash Screen'),
+          ),
+        )
       ),
     );
   }
