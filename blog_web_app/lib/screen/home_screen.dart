73a8630b1bb9a2a0c91b234465fa2369aa3ed914
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-19 08:19:43 +0900

    웹뷰 연동하기

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+// 웹뷰 플러그인
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   // 생성자
   HomeScreen({Key? key}) : super(key: key);
 
+  // 웹뷰 연동하기
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: WebView(
+        initialUrl: 'https://peterica.tistory.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
