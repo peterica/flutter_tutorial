eab89a7ab6c6f67f6c17f2ec963792551dba14d6
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-19 17:55:23 +0900

    8장 전자액자 - 상태바 색상 변경

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,11 +7,17 @@ class HomeScreen extends StatelessWidget {
   // Page View 구현하기
   @override
   Widget build(BuildContext context) {
+    // 상태바 색상 변경
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return Scaffold(
       body: PageView(
         children: [1,2,3,4,5]
             .map(
-                (number) => Image.asset('asset/img/image_$number.jpeg'),
+                (number) => Image.asset(
+                  'asset/img/image_$number.jpeg',
+                  fit: BoxFit.cover,
+                ),
         ).toList(),
       ),
     );
