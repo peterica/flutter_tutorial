b81e137f02e24453dcecf3ac3481cd6dc55df733
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-19 18:27:55 +0900

    8장 전자액자 - page컨트롤러 생성 및 에니메이션 적용

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async'; // Timer 참조 import
 
-class HomeScreen extends StatelessWidget {
+// StatefulWidget 바꾸기
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  // page 컨트롤러 생성
+  final PageController pageController = PageController();
+
+  // 타이머 추가
+  @override
+  void initState() {
+    super.initState(); // 부모 initState를 실행한다
+
+    Timer.periodic(
+      Duration(seconds: 3),
+      (timer) {
+
+        int? nextPage = pageController.page?.toInt();
+
+        // 페이지 null 예외처리
+        if (nextPage == null){
+          return;
+        }
+
+        // 로테이션 로직
+        if (nextPage == 4) {
+          nextPage=0;
+        } else {
+          nextPage++;
+        }
+
+        // 페이지 전환 에니메이션 적용
+        pageController.animateToPage(
+          nextPage,
+          duration: Duration(microseconds: 500),
+          curve: Curves.ease,);
+      },
+    );
+  }
 
   // Page View 구현하기
   @override
@@ -12,6 +55,7 @@ class HomeScreen extends StatelessWidget {
 
     return Scaffold(
       body: PageView(
+        controller: pageController, // PageController 주입
         children: [1,2,3,4,5]
             .map(
                 (number) => Image.asset(
