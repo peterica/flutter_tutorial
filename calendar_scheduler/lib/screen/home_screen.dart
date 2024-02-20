83c0833b0dcdc814d57fd1bd7a92232b32f4de71
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-20 22:44:27 +0900

    16장 일정 관리 앱 만들기 - 달력 구현하기, 주색상 설정

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/component/main_calendar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key:key);
@@ -6,7 +7,14 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(   // 시스템 UI 피해서 UI 구현
+        child: Column(  // 달력과 리스트를 세로로 배치
+          children: [
+            // 메인 달력 연결
+            MainCalendar(),
+          ],
+        ),
+      ),
     );
   }
 }
