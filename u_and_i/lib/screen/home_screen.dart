3470772a4ef06f09b3de35d01590dcf551cd02ba
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-20 09:03:10 +0900

    9장 만난 지 며칠 U&I - 홈 스크린 UI 구현하기(커플이미지구성)

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -3,13 +3,30 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  // 프로젝트 초기세팅
+  // 홈 스크린 UI 구현하기
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          // 위아래 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          //  반대축 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
+
       // https://api.flutter.dev/flutter/widgets/Text-class.html
       // 응용한 Text 출력
-      body: const Center(
+      /*body: const Center(
         child: SelectionArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -18,7 +35,31 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),*/
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('DDay Widget');
+  }
+}
+
+// 커플이미지 구성
+class _CoupleImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        'asset/img/middle_image.png',
+
+        // 화면 반만큼 높이 설정
+        height: MediaQuery.of(context).size.height / 2,
       ),
     );
   }
 }
+
+
