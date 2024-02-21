5aec98a7d347fbc6c822493a51eeb71d508c21ef
Peter Seo <ilovefran.ofm@gmail.com>
2024-02-21 16:11:52 +0900

    17장 일정 관리 앱 - 일정 데이터 읽기

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:calendar_scheduler/component/schedule_card.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
 import 'package:calendar_scheduler/const/colors.dart';
+import 'package:get_it/get_it.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -57,11 +59,39 @@ class _HomeScreenState extends State<HomeScreen> {
               count: 0,
             ),
             SizedBox(height: 8),
-            // 일정 카드
-            ScheduleCard(
-                startTime: 12,
-                endTime: 15,
-                content: '일정을 샘플로 만들어보기'
+            Expanded(   // 남는 공간을 모두 차지하기
+              // database의 데이터가 stream으로 넘어옴.
+              child: StreamBuilder<List<Schedule>>(
+                stream: GetIt.I<LocalDatabase>().watchSchedules(selectedDate),
+                builder: (context, snapshot) {
+
+                  // 데이터가 없을 때 빈 컨테이너 전달
+                  if(!snapshot.hasData){
+                    return Container();
+                  }
+
+                  // 화면 랜더링
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+
+                      final schedule = snapshot.data![index];
+
+                      // UI개선, 좌우로 패딩
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: 8, left: 8
+                        ),
+                        child: ScheduleCard(
+                          startTime: schedule.startTime,
+                          endTime: schedule.endTime,
+                          content: schedule.content,
+                        ),
+                      );
+                    },
+                  );
+                },
+              )
             ),
           ],
         ),
